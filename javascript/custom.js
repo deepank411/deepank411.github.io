33bd73ed3c9fa58f1a5d43c0fd6595b73fdb6f8a
Deepank Agarwal <deepank411@gmail.com>
2016-10-07 08:34:40 +0530

    contact form

--- javascript/custom.js
+++ javascript/custom.js
@@ -9,3 +9,10 @@ var scene1 = new ScrollMagic.Scene({
 })
 .setPin("#pinned-element1")
 .addTo(controller);
+var scene1 = new ScrollMagic.Scene({
+   triggerElement: "#pinned-trigger1",
+   triggerHook: 0,
+   reverse: true
+})
+.setPin("#pinned-element2")
+.addTo(controller);
