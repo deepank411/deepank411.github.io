e1f7a837926f5dd3a0311c2c0e4b9751167a2afc
Deepank Agarwal <deepank411@gmail.com>
2016-10-16 12:59:17 +0530

    integrated isotope

--- javascript/custom.js
+++ javascript/custom.js
@@ -16,3 +16,31 @@ var scene1 = new ScrollMagic.Scene({
 })
 .setPin("#pinned-element2")
 .addTo(controller);
+
+
+
+// external js: isotope.pkgd.js
+
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+});
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    console.log(filterValue);
+    // filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $( this ).addClass('is-checked');
+    });
+});
