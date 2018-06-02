ee3026ef854bf4b9a93f4789b7ce82fa8ee3c065
Deepank Agarwal <deepank411@gmail.com>
2018-06-02 18:49:32 +0530

    animations integrated

--- javascript/custom.js
+++ javascript/custom.js
@@ -3,66 +3,52 @@
 // 	$(".se-pre-con").fadeOut("slow");
 // });
 
-function isScrolledIntoView(elem) {
-	console.log('asd', elem, $(elem).offset().top);
-	var docViewTop = $(window).scrollTop();
-	var docViewBottom = docViewTop + $(window).height();
-
-	var elemTop = $(elem).offset().top;
-	var elemBottom = elemTop + $(elem).height();
-
-	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-$(window).scroll(function () {
-	// $('.hello-title').each(function () {
-	// 	if (isScrolledIntoView(this)) {
-	// 		$(this).addClass('animated fadeInDown');
-	// 	}
-	// });
-	// $('.left-area').each(function () {
-	// 	if (isScrolledIntoView(this)) {
-	// 		$(this).addClass('animated fadeInLeft');
-	// 	}
-	// });
-	// $('.right-area').each(function () {
-	// 	if (isScrolledIntoView(this)) {
-	// 		$(this).addClass('animated fadeInRight');
-	// 	}
-	// });
-});
-
 console.log('%c Like what you see? If you\'re looking for the code, find it here: https://github.com/deepank411/deepank411.github.io', 'font-size: 25px; color: #666;');
 
-$('.modal-trigger').leanModal();
+jQuery(function ($) {
 
-// external js: isotope.pkgd.js
-// init Isotope
-var $grid = $('.grid').isotope({
-	itemSelector: '.grid-item',
-	layoutMode: 'fitRows'
-});
+	function afterReveal(el) {
+		el.addEventListener('animationstart', function (event) {
+			$('.wow').each(function () {
+				$(this).css('opacity', 1);
+			});
+		});
+	}
 
-// bind filter button click
-$('#filters').on( 'click', 'button', function() {
-	var filterValue = $( this ).attr('data-filter');
-	console.log(filterValue);
-	// use filterFn if matches value
-	// filterValue = filterFns[ filterValue ] || filterValue;
-	$grid.isotope({ filter: filterValue });
-});
+	new WOW({
+		callback: afterReveal
+	}).init();
 
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-	var $buttonGroup = $( buttonGroup );
-	$buttonGroup.on( 'click', 'button', function() {
-		$buttonGroup.find('.is-checked').removeClass('is-checked');
-		$( this ).addClass('is-checked');
+	$('.modal-trigger').leanModal();
+
+	// external js: isotope.pkgd.js
+	// init Isotope
+	var $grid = $('.grid').isotope({
+		itemSelector: '.grid-item',
+		layoutMode: 'fitRows'
 	});
-});
 
-$('.screenshots').lightGallery({
-	thumbnail:true,
-	animateThumb: false,
-	showThumbByDefault: false
+	// bind filter button click
+	$('#filters').on( 'click', 'button', function() {
+		var filterValue = $( this ).attr('data-filter');
+		console.log(filterValue);
+		// use filterFn if matches value
+		// filterValue = filterFns[ filterValue ] || filterValue;
+		$grid.isotope({ filter: filterValue });
+	});
+
+	// change is-checked class on buttons
+	$('.button-group').each( function( i, buttonGroup ) {
+		var $buttonGroup = $( buttonGroup );
+		$buttonGroup.on( 'click', 'button', function() {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$( this ).addClass('is-checked');
+		});
+	});
+
+	$('.screenshots').lightGallery({
+		thumbnail:true,
+		animateThumb: false,
+		showThumbByDefault: false
+	});
 });
